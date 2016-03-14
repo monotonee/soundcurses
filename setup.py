4f9f233933316159c732587502361364dcd5138b
monotonee <monotonee@tuta.io>
2016-03-14 15:04:14 -0400

    Added setup.py file. Still incomplete.

---
+++ setup.py
@@ -0,0 +1,39 @@
+# Setuptools
+# from setuptools import setup, find_packages
+
+# setup(
+    # # Standard data
+    # name=  'soundcurses',
+    # version = '0.1.0',
+    # packages = find_packages(),
+    # install_requires = [
+        # 'pinject',
+        # 'soundcoud',
+    # ],
+
+    # # PyPI metadata
+    # author = 'monotonee',
+    # author_email = 'monotonee@tuta.io',
+    # description = 'A curses-based front end for the SoundCloud',
+    # license = 'MIT',
+    # keywords = 'cli curses  music ncurses soundcloud terminal',
+    # url = '',
+# )
+
+# Distutils
+# Example: https://github.com/google/pinject/blob/master/setup.py
+from distutils.core import setup
+
+setup(
+    # Standard data
+    name=  'soundcurses',
+    version = '0.1.0',
+    packages = ['soundcurses'],
+
+    # PyPI metadata
+    author = 'monotonee',
+    author_email = 'monotonee@tuta.io',
+    description = 'A curses-based front end for the SoundCloud',
+    license = 'MIT',
+    url = '',
+)
