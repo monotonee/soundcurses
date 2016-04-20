ade20ffee9a03e6c2486e9402daeee125fb31ebb
monotonee <monotonee@tuta.io>
2016-04-20 16:00:20 -0400

    Changed names, added comments, readded positional parameters.

--- soundcurses/view.py
+++ soundcurses/view.py
@@ -10,16 +10,19 @@ import abc
 
 class CursesView:
 
-    def __init__(self, curses, locale, stdscr):
-        """ Initialize the curses standard (main) screen and window hierarchy.
+    def __init__(
+        self, curses, locale,
+        window_stdscr, window_header, window_nav, window_content):
+        """ Construct the curses standard (main) screen and window hierarchy.
 
         curses - The curses library interface.
         locale - A reference to the Python standard library locale interface.
-        stdscr - A reference to a curses standard screen (main window).
+        window_stdscr - A reference to a curses standard screen (main window).
             Needed so that window refresh order can be properly maintained and
             UI events can be captured. Traditional "stdscr" nomenclature used
             to facilitate readability among those who are already familiar with
-            the C curses library.
+            the C curses library. Prefixed with "window" to denote custom
+            wrapper class around the raw curses stdscr.
 
         _character_encoding - Saves a reference to the character encoding used
             if it becomes necessary to convert byte streams into characters.
@@ -27,31 +30,24 @@ class CursesView:
         _locale - Displayed directly underneath the first bar. Often
             displays a breadcrumb-like hierarchy of current resources, letting
             a user know where one is in the SoundCloud resource tree.
-        _stdscr - The main window. Displays resource lists such as
+        _window_stdscr - The main window. Displays resource lists such as
             SoundCloud stream content, playlists, tracks, etc.
 
         """
-
         # Declare instance attributes.
         self._character_encoding = None
         self._curses = curses
         self._locale = locale
-        self._stdscr = stdscr
+        self._window_stdscr = window_stdscr
+        self._window_header = window_header
+        self._window_nav = window_nav
+        self._window_content = window_content
 
         # Gather information and establish initial instance state.
         self._set_character_encoding()
         self._configure_curses()
+        self._configure_subwindows()
         self._render()
-
-    def _set_character_encoding(self):
-        """ Determine environment locale and get encoding.
-
-        See https://docs.python.org/3.5/library/curses.html
-
-        """
-        # Set current locale to user default as specified in LANG env variable.
-        self._locale.setlocale(self._locale.LC_ALL, '')
-        self._character_encoding = self._locale.getpreferredencoding()
 
     def _configure_curses(self):
         """ Immediately assumes control of the current TUI window.
@@ -76,11 +72,33 @@ class CursesView:
         self._curses.init_pair(
             1, self._curses.COLOR_WHITE, self._curses.COLOR_BLUE)
 
+    def _configure_subwindows(self):
+
+
     def _render(self):
         """ Render virtual curses state to physical screen.
 
         """
         self._curses.doupdate()
+
+    def _set_character_encoding(self):
+        """ Determine environment locale and get encoding.
+
+        See https://docs.python.org/3.5/library/curses.html
+
+        """
+        # Set current locale to user default as specified in LANG env variable.
+        self._locale.setlocale(self._locale.LC_ALL, '')
+        self._character_encoding = self._locale.getpreferredencoding()
+
+    def start_input_polling(self):
+        """ Starts polling for input from curses windows.
+
+        """
+        while True:
+            char_code_point = self._window_stdscr.get_input()
+            if char_code_point == ord('q'):
+                break
 
     def destroy(self):
         """ Relinquish control of standard screen.
@@ -97,15 +115,13 @@ class CursesView:
 class CursesWindow(metaclass=abc.ABCMeta):
     """ Defines an abstract base class that encapsulates a layout region.
 
-    These are passed to the curses view which then manipulates the windows'
-    content.
-
     """
 
     def __init__(self, curses, window):
-        """ Initializer.
+        """ Constructor.
 
         curses - The curses library interface.
+        window - A raw curses window object.
 
         """
         # Retain passed arguments.
@@ -126,12 +142,12 @@ class CursesWindow(metaclass=abc.ABCMeta):
         Sets initial window state such as borders, colors, initial content, etc.
         Only called during object construction.
 
-        Warning: make sure to call _update_virtual_state() if necessary.
+        Warning: make sure to call update_virtual_state() if necessary.
 
         """
         pass
 
-    def _update_virtual_state(self):
+    def update_virtual_state(self):
         """ Writes window state to curses' virtual screen state.
 
         """
@@ -145,19 +161,21 @@ class StdscrWindow(CursesWindow):
     """
 
     def _configure_window(self):
-        """ Perform an empty update so that subsequent calls to refresh don't
-        cause stdscr to overlap other windows.
+        """ Perform an empty update so that subsequent calls to refresh, either
+        explicit or implicit (such as in window.getkey()) don't cause stdscr to
+        overlap other windows.
+
+        Called in constructor.
 
         """
         self._window.nodelay(True)
-        self._update_virtual_state()
 
-    def get_key(self):
+    def get_input(self):
         """ Wrapper for internal curses window instance method.
 
         """
 
-        return self._window.getkey()
+        return self._window.getch()
 
 
 class HeaderWindow(CursesWindow):
@@ -169,7 +187,6 @@ class HeaderWindow(CursesWindow):
         self._window.bkgd(' ', self._curses.color_pair(1))
         self._window.addstr(
             1, 2, 'Current user: Monotonee', self._curses.A_BOLD)
-        self._update_virtual_state()
 
 
 class NavWindow(CursesWindow):
@@ -179,8 +196,6 @@ class NavWindow(CursesWindow):
 
     def _configure_window(self):
         self._window.border()
-        self._update_virtual_state()
-
 
 class ContentWindow(CursesWindow):
     """ A curses window that manages the content window.
@@ -189,7 +204,6 @@ class ContentWindow(CursesWindow):
 
     def _configure_window(self):
         self._window.border()
-        self._update_virtual_state()
 
 
 
